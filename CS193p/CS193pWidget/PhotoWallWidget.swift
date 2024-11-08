dd2b93e8808cd6900cd1dd90410527fb8b6dcb33
DerainZhou <derainzhou@gmail.com>
2024-11-08 13:54:16 +0800

    update: 照片墙支持浏览下一张

--- CS193p/CS193pWidget/PhotoWallWidget.swift
+++ CS193p/CS193pWidget/PhotoWallWidget.swift
@@ -11,94 +11,198 @@
 import WidgetKit
 import SwiftUI
 import Photos
- 
- 
-// MARK: - PhotoWallProvider
-struct PhotoWallProvider: TimelineProvider {
-    public typealias Entry = PhotoWallEntry
+import AppIntents
+
+// MARK: - PhotoOpIntent
+enum PhotoOpType: String {
+    case save = "save"
+    case delete = "delete"
+}
+struct PhotoPanelButton: View {
+    private let type: PhotoOpType
     
-    func placeholder(in context: Context) -> PhotoWallEntry {
-        PhotoWallEntry(date: Date.now, photoIdentifiers: [])
+    init(type: PhotoOpType) {
+        self.type = type
     }
     
-    func getSnapshot(in context: Context, completion: @escaping (PhotoWallEntry) -> Void) {
-        let entry = PhotoWallEntry(date: Date.now, photoIdentifiers: [])
-        completion(entry)
+    var body: some View {
+        if #available(iOS 17.0, macOS 14.0, *) {
+            // Button
+            Button(intent: PhotoOpIntent(type: type.rawValue)) {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.red)
+                        .frame(width: 80, height: 40)
+                    
+                    Text(type.rawValue)
+                        .font(.system(size: 14))
+                        .foregroundColor(.white)
+                }
+            }
+            .buttonStyle(.plain)
+        }
+    }
+}
+
+class PhotoWallManager {
+    public static let shared = PhotoWallManager()
+    public var idx = 0
+    private var identifiers = [String]()
+    
+    init() {
+        identifiers = fetchImages()
     }
     
-    func getTimeline(in context: Context, completion: @escaping (Timeline<PhotoWallEntry>) -> Void) {
+    private func fetchImages() -> [String] {
         let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
         
         let assets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
         var identifiers: [String] = []
-        
-        // 只存储标识符
         assets.enumerateObjects { asset, _, _ in
             identifiers.append(asset.localIdentifier)
         }
-        
-        let entry = PhotoWallEntry(
-            date: Date(),
-            photoIdentifiers: identifiers
-        )
-        
-        let timeline = Timeline(entries: [entry], policy: .atEnd)
-        completion(timeline)
+        return identifiers
     }
-}
-
-
-// MARK: - CaffeineTrackerWidget
-
-struct PhotoWallEntry: TimelineEntry {
-    let date: Date
-    let photoIdentifiers: [String]  // 存储照片标识符
-}
-
-struct PhotoWallWidgetEntryView: View {
-    @Environment(\.widgetFamily) var family // 获取 widget 尺寸
-    var entry: PhotoWallEntry
     
-    func loadImage(identifier: String) -> UIImage? {
+    public func loadImage(identifier: String) -> UIImage? {
         let result = PHAsset.fetchAssets(withLocalIdentifiers: [identifier], options: nil)
         guard let asset = result.firstObject else { return nil }
         
         var resultImage: UIImage?
         let options = PHImageRequestOptions()
         options.isSynchronous = true
-        options.deliveryMode = .opportunistic  // 改用高质量图片
+        options.deliveryMode = .highQualityFormat
         
-        // 根据 widget 尺寸请求合适大小的图片
-        // let targetSize = family == .systemSmall ? CGSize(width: 158, height: 158) : CGSize(width: 338, height: 158)
-        let targetSize = CGSize(width: 300, height: 300)
+        let size = 300
+        let targetSize = CGSize(width: size, height: size)
         PHImageManager.default().requestImage(
             for: asset,
             targetSize: targetSize,
             contentMode: .aspectFill,
             options: options
         ) { image, _ in
-            resultImage = image
+            resultImage = image?.jpegData(compressionQuality: 0.7)
+                            .flatMap(UIImage.init)
         }
         
         return resultImage
     }
     
+    public func save() {
+        idx += 1
+    }
+    
+    public func delete() {
+        // 将照片放到回收站
+        // 展示下一张照片
+        idx += 1
+    }
+    
+    public func currentPhoto() -> String? {
+        guard (0..<identifiers.count).contains(idx) else {
+            return nil
+        }
+        return identifiers[idx]
+    }
+}
+
+struct PhotoOpIntent: AppIntent {
+    static var title: LocalizedStringResource = "PhotoOpIntent"
+    static var description = IntentDescription("PhotoOpIntent.")
+    
+    @Parameter(title: "Photo op type")
+    var type: String
+
+    init() {}
+    init(type: String) {
+        self.type = type
+    }
+    
+    func perform() async throws -> some IntentResult {
+        /// 更新数据
+        let type = PhotoOpType(rawValue: type)
+        switch type {
+        case .save: PhotoWallManager.shared.save()
+        case .delete: PhotoWallManager.shared.delete()
+        default: break
+        }
+        return .result()
+    }
+}
+
+// MARK: - PhotoPreviewView 照片预览控件
+struct PhotoPreviewView: View {
+    let identifier: String?
+    @Environment(\.widgetFamily) var family
+    
+    init(identifier: String?) {
+        self.identifier = identifier
+    }
+    
     var body: some View {
-        let firstIdentifier = entry.photoIdentifiers.first
-        if let firstIdentifier = firstIdentifier,
-           let image = loadImage(identifier: firstIdentifier) {
+        if let identifier = identifier,
+           let image = PhotoWallManager.shared.loadImage(identifier: identifier) {
             Image(uiImage: image)
                 .resizable()
-                .aspectRatio(contentMode: .fit) // 填充模式
-                .frame(maxWidth: .infinity, maxHeight: .infinity) // 填充整个空间
-                .clipped() // 裁剪超出部分
-                .containerBackground(.fill.tertiary, for: .widget)
+                .aspectRatio(contentMode: .fit)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .clipped()
         } else {
-            Text(firstIdentifier ?? "no image")
-                .containerBackground(.fill.tertiary, for: .widget)
+            Text(identifier ?? "no image")
+                .font(.system(size: 14))
+                .foregroundColor(.gray)
         }
-        
+    }
+}
+    
+ 
+// MARK: - PhotoWallWidget
+struct PhotoWallProvider: TimelineProvider {
+    public typealias Entry = PhotoWallEntry
+    
+    func placeholder(in context: Context) -> PhotoWallEntry {
+        PhotoWallEntry(date: Date.now)
+    }
+    
+    func getSnapshot(in context: Context, completion: @escaping (PhotoWallEntry) -> Void) {
+        let entry = PhotoWallEntry(date: Date.now)
+        completion(entry)
+    }
+    
+    func getTimeline(in context: Context, completion: @escaping (Timeline<PhotoWallEntry>) -> Void) {
+        let entry = PhotoWallEntry(date: Date())
+        let timeline = Timeline(entries: [entry], policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+
+struct PhotoWallEntry: TimelineEntry {
+    let date: Date
+}
+
+struct PhotoWallWidgetEntryView: View {
+    @Environment(\.widgetFamily) var family // 获取 widget 尺寸
+    var entry: PhotoWallEntry
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            PhotoPreviewView(identifier: PhotoWallManager.shared.currentPhoto())
+            VStack() {
+                Spacer()
+                HStack {
+                    Spacer()
+                    PhotoPanelButton(type: .save)
+                    Spacer()
+                    PhotoPanelButton(type: .delete)
+                    Spacer()
+                }
+                Spacer()
+                    .frame(height: 20)
+            }
+        }
+        .containerBackground(.fill.tertiary, for: .widget)
     }
 }
 
@@ -109,7 +213,7 @@ struct PhotoWallWidget: Widget {
         }
         .configurationDisplayName("照片墙")
         .description("显示最新照片")
-        .supportedFamilies([.systemSmall, .systemMedium, .systemLarge]) // 支持的尺寸
+        .supportedFamilies([.systemLarge]) // 支持的尺寸
         .contentMarginsDisabled()
     }
 }
@@ -124,7 +228,7 @@ struct PhotoWallWidget: Widget {
 
 struct PhotoWallWidget_Previews: PreviewProvider {
     static var previews: some View {
-        let view = PhotoWallWidgetEntryView(entry: PhotoWallEntry(date: .now, photoIdentifiers: []))
+        let view = PhotoWallWidgetEntryView(entry: PhotoWallEntry(date: .now))
         view.previewContext(WidgetPreviewContext(family: .systemMedium))
     }
 }
